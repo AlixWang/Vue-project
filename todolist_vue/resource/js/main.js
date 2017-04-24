25015a2886d9c1ab3719a8f319f12617c7082e89
alixwang <alixwang@alixwangdeMacBook-Pro.local>
2017-04-24 21:40:05 +0800

    + seletct-component-vue

--- todolist_vue/resource/js/main.js
+++ todolist_vue/resource/js/main.js
@@ -3,44 +3,66 @@ if (localStorage.getItem('todolist')) {
 } else {
     var todoList = {
         todo: [],
-        compt: []
+        compt: [],
+        editable:false
     }
 }
 
 new Vue({
     el: '#main',
     data: todoList,
+    watch:{
+        todo:{
+            handler:function(){
+                console.log('todolist change');
+                setlocalStorage(todoList);
+            },
+            deep:true
+        },
+        compt:{
+            handler:function(){
+                console.log('todolist change');
+                setlocalStorage(todoList);
+            },
+            deep:true
+        }
+    },
     methods: {
         addtodo: function (ev) {
-            // console.log(ev.currentTarget.parentNode.querySelector('input').value);
+             console.log(ev.currentTarget.parentNode.querySelector('input').value);
             if (ev.currentTarget.parentNode.querySelector('input').value) {
                 this.todo.push({ things: ev.currentTarget.parentNode.querySelector('input').value });
-                ev.currentTarget.parentNode.querySelector('input').value = ' ';
-                setlocalStorage(todoList);
+                ev.currentTarget.parentNode.querySelector('input').value = '';
+                
             }
             
         },
+        editetodo:function(item,ev){
+            console.log(item);
+            ev.currentTarget.contentEditable = true;
+        },
+        makesure:function(item,ev){
+            ev.currentTarget.contentEditable = false;
+            
+        }
+        ,
         deletetodo: function (item, ev) {
             console.log(item);
             this.todo.splice(this.todo.indexOf(item), 1);
-            setlocalStorage(todoList);
         },
         deletecompt: function (item, ev) {
             console.log(item);
             this.compt.splice(this.compt.indexOf(item), 1);
-            setlocalStorage(todoList);
         },
         tocompt: function (item, ev) {
             console.log(item);
             this.compt.unshift(item);
             this.todo.splice(this.todo.indexOf(item), 1);
-            setlocalStorage(todoList);
         },
         retodo: function (item, ev) {
             console.log(item);
             this.todo.unshift(item);
             this.compt.splice(this.compt.indexOf(item), 1);
-            setlocalStorage(todoList);
 
         }
     }
